d6b2139c73a066ec4c983eaefe0b7a9d2875f1c4
bgtip <bendik@gjovikli.no>
2016-10-21 10:53:23 +0200

    v0.0.0.2.3.5
    music now plays on options menu

--- Tst/Form2.vb
+++ Tst/Form2.vb
@@ -24,7 +24,7 @@
     Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click 'Går til Options menyen
         Form3.Show()
         Me.Visible = False
-        My.Computer.Audio.Stop()
+        'My.Computer.Audio.Stop()
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click 'Lukker spillet
--- Tst/Form3.vb
+++ Tst/Form3.vb
@@ -2,6 +2,11 @@
 Public Class Form3
     Public SIZE2 As Integer = 40
     Public TILE_SIZE2 As Integer = 10
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'My.Computer.Audio.Play("game-menu.wav",
+        'AudioPlayMode.BackgroundLoop)
+    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'går tilbake til hovedmenyen
         Form2.Show()
         Me.Visible = False
@@ -12,6 +17,7 @@ Public Class Form3
         TILE_SIZE2 = 400 / 50
         Form1.Show()
         Me.Visible = False
+        My.Computer.Audio.Stop()
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click ' skyfter til mode 2
@@ -19,6 +25,7 @@ Public Class Form3
         TILE_SIZE2 = 400 / 60
         Form1.Show()
         Me.Visible = False
+        My.Computer.Audio.Stop()
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click ' skyfter til mode 3
@@ -26,10 +33,12 @@ Public Class Form3
         TILE_SIZE2 = 400 / 70
         Form1.Show()
         Me.Visible = False
+        My.Computer.Audio.Stop()
     End Sub
 
 
     Private Sub OnApplicationExit(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Closing
         Application.Exit()
     End Sub
+
 End Class
