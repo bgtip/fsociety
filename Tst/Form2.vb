9812445927a010f0ae86b465626e1f9a81089716
crispocolus <kristoffer.selbekk.hille@gmail.com>
2016-10-26 22:15:00 +0200

    v0.0.0.2.5.5
    fixed bug

--- Tst/Form2.vb
+++ Tst/Form2.vb
@@ -7,8 +7,7 @@
     End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        My.Computer.Audio.Play("sound/music/main.wav",
-        AudioPlayMode.BackgroundLoop)
+        My.Computer.Audio.Play("sound/music/main.wav", AudioPlayMode.BackgroundLoop)
 
         Dim conf As String
         conf = My.Computer.FileSystem.ReadAllText("config.txt")
