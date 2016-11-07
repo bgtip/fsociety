9526bed1c2d51af08bc9e5915a8df4dc87f37995
John Gullaksen <zhashade@gmail.com>
2016-11-07 15:40:35 +0100

    v0.0.1.5.7
    Every window is now not resizable. Except the score one.

--- Tst/Form1.Designer.vb
+++ Tst/Form1.Designer.vb
@@ -37,10 +37,9 @@ Partial Class Form1
         '
         'Canvas
         '
-        Me.Canvas.Location = New System.Drawing.Point(100, 79)
-        Me.Canvas.Margin = New System.Windows.Forms.Padding(4)
+        Me.Canvas.Location = New System.Drawing.Point(75, 64)
         Me.Canvas.Name = "Canvas"
-        Me.Canvas.Size = New System.Drawing.Size(533, 492)
+        Me.Canvas.Size = New System.Drawing.Size(400, 400)
         Me.Canvas.TabIndex = 0
         '
         'Score
@@ -48,10 +47,9 @@ Partial Class Form1
         Me.Score.AutoSize = True
         Me.Score.BackColor = System.Drawing.Color.Transparent
         Me.Score.ForeColor = System.Drawing.Color.White
-        Me.Score.Location = New System.Drawing.Point(688, 79)
-        Me.Score.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Score.Location = New System.Drawing.Point(516, 64)
         Me.Score.Name = "Score"
-        Me.Score.Size = New System.Drawing.Size(65, 17)
+        Me.Score.Size = New System.Drawing.Size(50, 13)
         Me.Score.TabIndex = 1
         Me.Score.Text = "Poeng: 0"
         '
@@ -68,10 +66,9 @@ Partial Class Form1
         Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.ForeColor = System.Drawing.Color.Transparent
-        Me.Button1.Location = New System.Drawing.Point(691, 535)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Location = New System.Drawing.Point(518, 435)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(126, 36)
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
         Me.Button1.TabIndex = 2
         Me.Button1.UseVisualStyleBackColor = False
         '
@@ -83,24 +80,25 @@ Partial Class Form1
         Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
         Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Location = New System.Drawing.Point(691, 464)
+        Me.Button2.Location = New System.Drawing.Point(518, 377)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(153, 41)
+        Me.Button2.Size = New System.Drawing.Size(115, 33)
         Me.Button2.TabIndex = 3
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Tst.My.Resources.Resources.formOne2
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1016, 647)
+        Me.ClientSize = New System.Drawing.Size(762, 526)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Score)
         Me.Controls.Add(Me.Canvas)
-        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Form1"
         Me.Text = "fsociety27.dat - game"
         Me.ResumeLayout(False)
